a799e927c5184df06670d29fa3102f200b8f3803
kim hyun sik <devconf5296@gmail.com>
2022-01-21 19:22:41 +0900

    initial

---
+++ src/main/java/com/bootcampbackend/BootCampBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bootcampbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCampBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootCampBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bootcampbackend/BootCampBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcampbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootCampBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
