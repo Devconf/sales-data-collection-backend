6a5c78bb500e16dc750321f414a03a7b33a7c784
kim hyun sik <devconf5296@gmail.com>
2022-02-09 01:36:26 +0900

    Feat:Solve CORS problem and Role check from token received in HTTP header

--- src/main/java/com/bootcampbackend/common/config/SecurityConfig.java
+++ src/main/java/com/bootcampbackend/common/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.bootcampbackend.common.config;
 import com.bootcampbackend.common.filter.JwtAuthenticationFilter;
 import com.bootcampbackend.user.application.common.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -10,6 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -22,6 +26,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http.httpBasic()
         .disable() // security에서 기본으로 생성하는 login페이지 사용 안 함
+        .cors()
+        .configurationSource(corsConfigurationSource())
+        .and()
         .csrf()
         .ignoringAntMatchers("/h2-console/**")
         .disable() // csrf 사용 안 함 == REST API 사용하기 때문에
@@ -31,12 +38,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .authorizeRequests() // 다음 리퀘스트에 대한 사용권한 체크
         .antMatchers("/login", "/signup", "/h2-console/**")
         .permitAll() // 가입 및 인증 주소는 누구나 접근가능
-        .antMatchers("/sales")
+        .antMatchers("/sales/**")
+        // hasRole에서 ROLE prefix를 붙여주기때문에 jwtTokenProvider에서 new
+        // SimpleGrantedAuthority("ROLE_USER")로 권한을 넣어줘야한다.
         .hasRole("USER") // 그외 나머지 요청은 모두 인증된 회원만 접근 가능
         .and()
         .addFilterBefore(
             new JwtAuthenticationFilter(jwtTokenProvider),
             UsernamePasswordAuthenticationFilter.class);
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+
+    configuration.addAllowedOriginPattern("http://localhost:3000");
+    configuration.addAllowedHeader("*");
+    configuration.addAllowedMethod("*");
+    configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 
   public void configure(WebSecurity web) throws Exception {
